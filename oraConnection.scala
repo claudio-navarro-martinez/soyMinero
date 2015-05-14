76eaf80bac93322f884c6b34f3dd5176bcb6e2db
claudio-navarro-martinez <claudio.oami@gmail.com>
2015-05-14 14:38:09 +0100

    Update oraConnection.scala

--- oraConnection.scala
+++ oraConnection.scala
@@ -1,27 +1,29 @@
-import java.sql.Connection
-import java.sql.ResultSet
- 
-import oracle.jdbc.pool.OracleDataSource
+import scala.language.postfixOps 
 
-object soyMinero {
+import java.sql.{Connection, DriverManager, ResultSet} 
+import oracle.jdbc.OracleTypes 
+ 
+
+object oracleClientLogMiner {
   def main(args: Array[String]) {
       if (args.length != 4) {
         println("Args: username pwd hostname servicename")
         System.exit(1)
      }
      
-  val ods = new OracleDataSource()
-  ods.setUser(args(0))
-  ods.setPassword(args(1))
-  ods.setURL("jdbc:oracle:thin:@" + args(2)+":1521/"+args(3))
-  val con = ods.getConnection()
+  val oracleDataSource = new OracleDataSource()
+  oracleDataSource setUser(args(0))
+  oracleDataSource setPassword(args(1))
+  oracleDataSource setURL("jdbc:oracle:thin:@" + args(2)+":1521/"+args(3))
+  val connection = oracleDataSource getConnection
   
-  val s1 = con.createStatement()
-  s1.setFetchSize(1000)
-  val rs = s1.executeQuery("Select /*fetchsize=" + s1.getFetchSize() + " */ * " + "from customers where rownum<= " + rows)
-  while (rs.next()) {
+  val stament1 = con createStatement
+  stament1 setFetchSize 10
+  val rs = s1 executeQuery "Select /*fetchsize=" + s1.getFetchSize() + " */ * " + "from customers where rownum<= " + rows
+  while (rs next()) {
          val c1 = rs.getString(1)
          val c2 = rs.getString(2)
   }
-  rs.close()
+  if (rs != null) 
+      rs.close()
 }  
