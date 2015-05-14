28b2fd99ea23e49723d1eaf0249c9d49e8b8b9c6
claudio-navarro-martinez <claudio.oami@gmail.com>
2015-05-14 17:02:00 +0100

    Update oraConnection.scala

--- oraConnection.scala
+++ oraConnection.scala
@@ -24,6 +24,33 @@ object oracleClientLogMiner {
          val c1 = rs.getString(1)
          val c2 = rs.getString(2)
   }
+  
+ /** Stored procedure */ 
+    
+ // ? indicates parameter - in, out and cursor. 
+  val callString = "{ call DBMS_LOGMNR.START_LOGMNR(STARTTIME => ’19-MAR-2013 14:02:14′, ENDTIME => SYSDATE, OPTIONS => DBMS_LOGMNR.DICT_FROM_ONLINE_CATALOG + DBMS_LOGMNR.CONTINUOUS_MINE ) }" 
+  val callableStatement = connection prepareCall callString 
+78       callableStatement.setInt(1, 7) // Sets first parameter to 7 
+79       callableStatement.setInt(2, 9) // Sets second parameter to 9 
+80       // Sets third parameter to output as cursor. 
+81       callableStatement registerOutParameter(3, OracleTypes.CURSOR) // or eg FLOAT 
+82    
+83       callableStatement execute 
+84       // Gets the returned value of the output parameter as an Object. 
+85       val resultObject = callableStatement getObject 3 // or eg getFloat 
+86    
+87       // Scala casting - returned Object to ResultSet. 
+88       var procRs: ResultSet = resultObject match { 
+89         case r: ResultSet => r; 
+90         case _ => null 
+91       } 
+92    
+93       while (procRs next) { 
+94         println(procRs getInt 1) 
+95       } 
+96    
+97       if (procRs != null) procRs.close 
+98       if (callableStatement != null) callableStatement.close 
   if (rs != null) 
       rs.close()
 }  
